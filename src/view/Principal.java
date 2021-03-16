ef80e90b550fe44d1c3f83cb7c4f99f99d8833f5
Karen <Karen@HTPIXS-DESKTOP>
2021-03-16 11:42:01 -0300

    EX04 Threads - Aplicação do Semaforo

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,13 +1,19 @@
 package view;
 
+import java.util.concurrent.Semaphore;
+
 import controller.CorridadeSapos;
 
 public class Principal {
-
+    
+	public static Semaphore semaforo;
+	
 	public static void main(String[] args) {
 		
+		semaforo = new Semaphore(1);
+		
 		for(int i = 0; i<5; i++) {
-	     	  Thread corridadesapo = new CorridadeSapos(i);
+	     	  Thread corridadesapo = new CorridadeSapos(i, semaforo);
 			  corridadesapo.start();		
 	    }
 		
